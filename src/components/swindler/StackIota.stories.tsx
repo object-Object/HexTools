8c76d6ed3daecfa27f538cca45d4dd39b93973f4
object-Object <object@objectobject.ca>
2026-02-16 17:21:00 -0500

    Add dnd-kit

--- src/components/swindler/StackIota.stories.tsx
+++ src/components/swindler/StackIota.stories.tsx
@@ -1,12 +1,22 @@
+import { DragDropProvider } from "@dnd-kit/react";
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import StackIota from "./StackIota";
 
 const meta = {
   component: StackIota,
+  args: {
+    id: 0,
+    index: 0,
+  },
+  render: (args) => (
+    <DragDropProvider>
+      <StackIota {...args} />
+    </DragDropProvider>
+  ),
 } satisfies Meta<typeof StackIota>;
-
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
--- src/components/swindler/StackIota.tsx
+++ src/components/swindler/StackIota.tsx
@@ -1,10 +1,21 @@
+import { useSortable } from "@dnd-kit/react/sortable";
 import { Group, TextInput } from "@mantine/core";
 import { IconGripVertical } from "@tabler/icons-react";
 
-export default function StackIota() {
+export interface StackIotaProps {
+  id: number;
+  index: number;
+}
+
+export default function StackIota({ id, index }: StackIotaProps) {
+  const { ref, handleRef, isDragging } = useSortable({ id, index });
+
   return (
-    <Group wrap="nowrap">
-      <IconGripVertical />
+    <Group ref={ref} wrap="nowrap">
+      <IconGripVertical
+        ref={handleRef}
+        style={{ cursor: isDragging ? "grabbing" : "grab" }}
+      />
       <TextInput style={{ flexGrow: 1 }} />
     </Group>
   );
