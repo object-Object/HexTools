63e06ea6ada47a9e64a71f33a0be0fc6ac9281e3
object-Object <object@objectobject.ca>
2026-05-01 01:24:13 -0400

    Add WebGL Inspector

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,6 +4,14 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: ["@storybook/addon-themes"],
   framework: "@storybook/react-vite",
+  previewHead: (head) => `
+    ${head}
+    ${
+      process.env.ENABLE_WEBGL_INSPECTOR
+        ? '<script src="https://object-object.github.io/WebGL-Inspector/core/embed.js"></script>'
+        : ""
+    }
+  `,
 };
 
 export default config;
